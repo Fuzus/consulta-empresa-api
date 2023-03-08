a0f30a9b3d31082c4ffc8510b7d98b872d96d03b
Fuzus <matheusdamata2000@gmail.com>
2023-03-08 15:51:10 -0300

    first commit

---
+++ src/main/java/br/com/fuzus/consultaempresaapi/ConsultaEmpresaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fuzus.consultaempresaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaEmpresaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaEmpresaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fuzus/consultaempresaapi/ConsultaEmpresaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fuzus.consultaempresaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaEmpresaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
